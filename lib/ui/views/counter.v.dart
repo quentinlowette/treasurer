643584b4c63054151f20e377498da7c58e70e94f
Quentin Lowette <quentin.lowette@gmail.com>
2020-02-19 21:52:12 +0100

    Application of the provider architecture

---
+++ lib/ui/views/counter.v.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:provider_architecture/provider_architecture.dart';
+import 'package:treasurer/core/viewmodels/counter.vm.dart';
+
+class CounterView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => ViewModelProvider<CounterViewModel>.withConsumer(
+    viewModel: CounterViewModel(),
+    builder: (context, model, child) => Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter Demo Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text('You have pushed the button this many times:'),
+            Text('${model.counter}', style: Theme.of(context).textTheme.headline4,)
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          model.increment();
+        },
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ),
+    )
+  );
+}
