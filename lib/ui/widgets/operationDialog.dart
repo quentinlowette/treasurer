b74e0c33725bae049a8f70a4891a7586125674f0
Quentin Lowette <quentin.lowette@gmail.com>
2020-02-23 10:54:44 +0100

    add operation dialog

---
+++ lib/ui/widgets/operationDialog.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:treasurer/core/models/operation.m.dart';
+
+class OperationDialog extends StatelessWidget {
+  final Operation operation;
+
+  OperationDialog({@required this.operation});
+
+  @override
+  Widget build(BuildContext context) {
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0)
+      ),
+      child: Container(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Text("${operation.description}", style: Theme.of(context).textTheme.headline5,),
+            SizedBox(height: 10.0,),
+            Text("${operation.date}"),
+            Text("${operation.amount}"),
+            Text("${operation.isCash}"),
+            Text("${operation.receiptPhotoPath}"),
+            // RaisedButton(
+            //   onPressed: () => Navigator.of(context).pop(),
+            //   child: Text("Close", style: Theme.of(context).accentTextTheme.button,),
+            //   color: Theme.of(context).accentColor,
+            // )
+          ],
+        ),
+      ),
+    );
+  }
+}
