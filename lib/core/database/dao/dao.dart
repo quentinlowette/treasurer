2199d2d15d27699a1e4de811a216e5da19c9e0c8
Quentin Lowette <quentin.lowette@gmail.com>
2020-05-08 22:27:04 +0200

    doc dao

--- lib/core/database/dao/dao.dart
+++ lib/core/database/dao/dao.dart
@@ -1,25 +1,36 @@
 import 'package:flutter/foundation.dart';
 
-/// Abstract Data-Access Object Class
+/// An abstract Data-Access Object.
+///
+/// Dao are used to interact with a table in the database.
 abstract class Dao<T> {
-  /// Getter for the table columns
-  @protected
-  String get columns =>
-      columnsDefinition.map((col) => col.join(" ")).toList().join(", ");
-
-  /// Description of the columns in the database that store the object
+  /// The description of the columns in the database that store the object
+  ///
+  /// The description is stored as a list of column representation.
+  /// These representations are encoded as a list of the column name and
+  /// the SQL parameters needed for the table's creation.
+  ///
+  /// Example:
+  /// ```dart
+  /// [
+  ///   ["foo", "INTEGER PRIMARY KEY"],
+  ///   ["bar", "REAL"],
+  ///   ["baz", "TEXT NOT NULL"]
+  /// ]
+  /// ```
   @protected
   List<List<String>> columnsDefinition;
 
-  /// Getter for the create table query
-  String get createTableQuery => """CREATE TABLE $tableName ($columns)""";
+  /// The SQLite creation query of the underlying table.
+  String get creationQuery => """CREATE TABLE $tableName
+      (${columnsDefinition.map((col) => col.join(" ")).toList().join(", ")})""";
 
-  /// Getter for the table name
+  /// The name of the underlying table.
   String tableName;
 
-  /// Converts a [Map<String, dynamic>] to a generic object
+  /// Converts a [Map<String, dynamic>] to a generic object.
   T fromMap(Map<String, dynamic> map);
 
-  /// Converts a generic object to a [Map<String, dynamic>]
+  /// Converts a generic object to a [Map<String, dynamic>].
   Map<String, dynamic> toMap(T object);
 }
